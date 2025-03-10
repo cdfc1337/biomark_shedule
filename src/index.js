74b024055ea2eadec49d927820de4ba05c7564c0
cdfc1337 <carlitos_coelha@hotmail.com>
2025-03-10 13:46:39 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'; // This line should point to the App component correctly.
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
